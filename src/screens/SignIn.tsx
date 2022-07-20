651959f28776b0c867afa2bd1efa32c7ae5e83ac
Matheus Fornari <matheusfornari@hotmail.com>
2022-07-20 18:21:47 -0300

    install phosphor

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,28 @@
-import { View, Text } from 'react-native';
+import { VStack, Heading } from 'native-base';
+import { Envelope, Key } from 'phosphor-react-native';
+
+import Logo from '../assets/logo_primary.svg';
+import { Input } from '../components/input';
 
 export function SignIn() {
   return (
-    <View>
-      <Text>Olá Matheusses</Text>
-    </View>);
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse a sua conta
+      </Heading>
+
+      <Input
+        placeholder="E-mail"
+        mb={4}
+        InputLeftElement={<Icon as={Envelope} />}
+      />
+      <Input
+        placeholder="Senha"
+        mb={4}
+        InputLeftElement={<Icon as={Key} />}
+      />
+    </VStack>
+  );
 }
